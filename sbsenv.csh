98080400f99557e65331aa15e2738d6c71d0cce2
Andrew Puckett <puckett@jlab.org>
2020-02-20 10:52:21 -0500

    Bringing cmake build/install of SBS-offline up to the standard of g4sbs

---
+++ sbsenv.csh
@@ -0,0 +1,30 @@
+#!/bin/csh
+
+#script to set up the environment for SBS-offline
+setenv SBS @CMAKE_INSTALL_PREFIX@
+
+if( ! ${?PATH} ) then
+    setenv PATH @CMAKE_INSTALL_FULL_BINDIR@
+else
+    setenv PATH @CMAKE_INSTALL_FULL_BINDIR@:${PATH}
+endif
+
+set OS=`uname -s`
+
+
+if( "$OS" == "Darwin" ) then # Mac OS: set DYLD_LIBRARY_PATH to library directory:
+    if(! ${?DYLD_LIBRARY_PATH}) then
+	setenv DYLD_LIBRARY_PATH @CMAKE_INSTALL_FULL_LIBDIR@
+    else
+	setenv DYLD_LIBRARY_PATH @CMAKE_INSTALL_FULL_LIBDIR@:${DYLD_LIBRARY_PATH}
+    endif
+endif
+
+# set LD_LIBRARY_PATH regardless of OS:
+if( ! ${?LD_LIBRARY_PATH}) then
+    setenv LD_LIBRARY_PATH @CMAKE_INSTALL_FULL_LIBDIR@
+else
+    setenv LD_LIBRARY_PATH @CMAKE_INSTALL_FULL_LIBDIR@:${LD_LIBRARY_PATH}
+endif
+
+
---
+++ sbsenv.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#script to set up the environment for SBS-offline
+export SBS=${CMAKE_INSTALL_PREFIX}
+
+if test "x$PATH" = "x" ; then
+    export PATH=${CMAKE_INSTALL_FULL_BINDIR}
+else
+    export PATH=${CMAKE_INSTALL_FULL_BINDIR}:$PATH
+fi
+
+OS=`uname -s`
+
+
+if [ "$OS" == "Darwin" ]
+then # Mac OS: set DYLD_LIBRARY_PATH to library directory:
+    if test "x$DYLD_LIBRARY_PATH" = "x"; then
+	export DYLD_LIBRARY_PATH=${CMAKE_INSTALL_FULL_LIBDIR}
+    else
+	export DYLD_LIBRARY_PATH=${CMAKE_INSTALL_FULL_LIBDIR}:$DYLD_LIBRARY_PATH
+    fi
+fi
+
+# set LD_LIBRARY_PATH regardless of OS:
+if test "x$LD_LIBRARY_PATH" = "x"; then
+    export LD_LIBRARY_PATH=${CMAKE_INSTALL_FULL_LIBDIR}
+else
+    export LD_LIBRARY_PATH=${CMAKE_INSTALL_FULL_LIBDIR}:$LD_LIBRARY_PATH
+fi
+
+
