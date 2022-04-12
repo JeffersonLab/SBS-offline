44a2c5ddd85e7b6033bff80d0319c9caffb65467
Andrew Puckett <puckett@jlab.org>
2022-04-12 18:02:30 -0400

    moved hodoscope clustering to CoarseProcess routine so we can use the hodoscope in tracking constraint calculation, also refined constraint point calculation and added database-configurable parameters to control relative weights of shower, preshower, and hodoscope in tracking constraint calculation

--- SBSBigBite.h
+++ SBSBigBite.h
@@ -137,12 +137,22 @@ protected:
   Double_t fBackConstraintX0; 
   Double_t fBackConstraintY0;
 
-    
+  //The following parameters are used to calculate the weights in the constraint point calculation:
+  Double_t fSigmaX_shower; //default = block size /12
+  Double_t fSigmaY_shower; //default = block size /12
+  Double_t fSigmaX_preshower; //default = block size /12
+  Double_t fSigmaY_preshower; //default = block size /12
+
+  Double_t fSigmaX_hodo; //default = bar vertical size / sqrt(12)
+  Double_t fSigmaY_hodo; //default = hit resolution from left/right time difference
+  
   //for output only... Vectors instead?
   std::vector<double> fFrontConstraintX;
   std::vector<double> fFrontConstraintY;
+  std::vector<double> fFrontConstraintZ;
   std::vector<double> fBackConstraintX;
   std::vector<double> fBackConstraintY;
+  std::vector<double> fBackConstraintZ;
     
   std::vector<double> fEpsEtotRatio;
   std::vector<double> fEtot;
