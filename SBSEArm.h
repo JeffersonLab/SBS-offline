ca6ee192954a1f7f0f4e10666f073b26cf0c9cde
Sean Jeffas <jeffas@ifarm1802.jlab.org>
2022-09-19 10:29:40 -0400

    Added calorimeter constraint functionality for SBS arm GEMs

--- SBSEArm.h
+++ SBSEArm.h
@@ -11,9 +11,26 @@ class SBSEArm : public THaSpectrometer {
 
     virtual Int_t FindVertices( TClonesArray& tracks );
     virtual Int_t TrackCalc();
+    virtual Int_t CoarseReconstruct();
 
     protected:
+    virtual Int_t ReadDatabase( const TDatime& date );
+
+    Double_t fFrontConstraintWidthX;
+    Double_t fFrontConstraintWidthY;
+    Double_t fBackConstraintWidthX;
+    Double_t fBackConstraintWidthY;
+    Double_t fFrontConstraintX0;
+    Double_t fFrontConstraintY0;
+    Double_t fBackConstraintX0; 
+    Double_t fBackConstraintY0;
+
+    TVector3 fGEMorigin;  //Absolute position of GEM origin relative to target center, in TARGET transport coordinates
+
+
+
     ClassDef(SBSEArm,0) // BigBite spectrometer
+
 };
 
 
