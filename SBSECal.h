5c8a3d8b2a72e93edc879e17a1b5e5fc098bbafd
Andrew Puckett <puckett@jlab.org>
2025-04-11 18:23:52 -0400

    Added CalcShowerCoord method to SBSECal for improved cluster position reconstruction

--- SBSECal.h
+++ SBSECal.h
@@ -27,6 +27,8 @@ public:
 
   inline void SetRefADCtimeGoodCluster(Double_t tval){ fRefADCtimeGoodCluster = tval; }
 
+  void CalcShowerCoord(); //Calculate shower coordinates
+  
 protected:
   // Bool_t fWithLED;
   // Int_t fLEDCrate;
@@ -40,7 +42,34 @@ protected:
   Double_t fAtimeMaxGoodCluster; //default to wide optn limits 
 
   // //Flag to filter clusters based on presence of at least one good TDC hit?
-  // Bool_t fRequireTDCGoodCluster;  
+  // Bool_t fRequireTDCGoodCluster;
+
+  //We'll need to add some output variables
+  //Actually no we won't. 
+  // We just need a public method to calculate and apply the shower shape corrections
+  // to the cluster position.
+  
+  // Adding parameters for shower shape corrections:
+
+  Bool_t fUseShowerShapeCorr; //Flag to correct ECAL cluster positions using "shower shape" profiles
+
+  Int_t fShowerShapeNbinsX;
+  Int_t fShowerShapeNbinsY;
+  Int_t fShowerShapeNbinsProf;
+  Double_t fShowerShapeMomMin; //default to -1
+  Double_t fShowerShapeMomMax; //default to +1
+
+  Double_t fShowerShapeXminProf;
+  Double_t fShowerShapeXmaxProf;
+  Double_t fShowerShapeYminProf;
+  Double_t fShowerShapeYmaxProf;
+  
+  Double_t fShowerShapeLxProf;
+  Double_t fShowerShapeLyProf;
+
+  std::vector<std::vector<Double_t> > fShowerProfilesX;
+  std::vector<std::vector<Double_t> > fShowerProfilesY;
+
   
   ClassDef(SBSECal,0)     // ECal detector class
 };
